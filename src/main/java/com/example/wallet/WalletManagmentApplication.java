0ca56608f19f1f1b8852b7412470fc1756c84f4e
Sergey <93572822+JusteRYT@users.noreply.github.com>
2024-11-19 12:49:03 +0300

    Initial commit

---
+++ src/main/java/com/example/wallet/WalletManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletManagmentApplication.class, args);
+	}
+
+}
